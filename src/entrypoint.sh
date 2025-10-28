fd53754a682c5b9fba5bd830050f975797dde377
charles25565 <charles4090@proton.me>
2025-10-28 16:22:18 -0400

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script exists because when Python is not running with -t, there will be no logs.
+# This script exists because the container is not running with -t, there will be no logs.
 # By disabling the Python buffer, we circumvent this.
 
 mitmproxy="/usr/local/bin/mitmdump"
@@ -8,4 +8,4 @@ entrypoint="main.py"
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
-exec $(head -n 1 "$mitmproxy" | sed "s/#!//g") -u "$mitmproxy" -s "$entrypoint"
+exec $(head -n 1 "$mitmproxy" | sed "s/#!//g" | cut -f1 -d" ") -u "$mitmproxy" -s "$entrypoint"
